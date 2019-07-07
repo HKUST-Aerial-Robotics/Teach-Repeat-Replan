46fb45de654c6a3e3cb9117596200f30974e86d1
USTfgaoaa <fgaoaa@connect.ust.hk>
2019-07-07 16:45:48 +0800

    fix cuda compile, fix trr_simulation script

--- trr_simulation.sh
+++ trr_simulation.sh
@@ -1,6 +1,5 @@
-roscore & sleep 1.5;
+roslaunch cascade_controller simulator.launch drone_number:=$i yaw:=true & sleep 4;
 rosrun joy joy_node & sleep 0.2;
-roslaunch cascade_controller simulator.launch drone_number:=$i yaw:=true & sleep 1;
 roslaunch trr_global_planner  map_generator.launch   & sleep 1;
 roslaunch trr_global_planner  global_planner.launch  & sleep 1;
 roslaunch trr_local_replanner local_replanner.launch & sleep 1;
