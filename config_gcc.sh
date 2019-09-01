7458725c64114d6b9d8e7427f0fcf72e0a519616
USTfgaoaa <fgaoaa@connect.ust.hk>
2019-09-01 23:16:08 +0800

    add config gcc script

---
+++ config_gcc.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y gcc-7 g++-7 & sleep 0.2;
+sudo update-alternatives --install -y /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 & sleep 0.2;
+sudo update-alternatives --install -y /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7;
+exit 1
--- install_tools.sh
+++ install_tools.sh
@@ -6,6 +6,4 @@ sudo apt-get install -y glpk-utils libglpk-dev & sleep 0.5;
 sudo apt-get install -y libcdd-dev & sleep 0.2;
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test & sleep 0.2;
 sudo apt-get update & sleep 0.2;
-sudo apt-get install -y gcc-7 g++-7 & sleep 0.2;
-sudo update-alternatives --install -y /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 & sleep 0.2;
-sudo update-alternatives --install -y /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7;
+
