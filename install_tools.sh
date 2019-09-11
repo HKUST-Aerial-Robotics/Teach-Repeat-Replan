bb5cd226d67dc21600a60f42f0897fb2918b95cb
Fei Gao <fgaoaa@connect.ust.hk>
2019-09-11 21:04:55 +0800

    Update install_tools.sh
    Fix install script 'sleep' issue.

--- install_tools.sh
+++ install_tools.sh
@@ -1,9 +1,9 @@
-sudo apt-get install -y ros-kinetic-joy & sleep 4;
-sudo apt-get install -y libnlopt-dev & sleep 0.5;
-sudo apt-get install -y libf2c2-dev & sleep 0.5;
-sudo apt-get install -y libarmadillo-dev & sleep 0.5;
-sudo apt-get install -y glpk-utils libglpk-dev & sleep 0.5;
-sudo apt-get install -y libcdd-dev & sleep 0.2;
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test & sleep 0.2;
-sudo apt-get update & sleep 0.2;
+sudo apt-get install -y ros-kinetic-joy && sleep 4;
+sudo apt-get install -y libnlopt-dev && sleep 0.5;
+sudo apt-get install -y libf2c2-dev && sleep 0.5;
+sudo apt-get install -y libarmadillo-dev && sleep 0.5;
+sudo apt-get install -y glpk-utils libglpk-dev && sleep 0.5;
+sudo apt-get install -y libcdd-dev && sleep 0.2;
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test && sleep 0.2;
+sudo apt-get update && sleep 0.2;
 
