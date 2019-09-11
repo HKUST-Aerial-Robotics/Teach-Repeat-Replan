1a537602be31cb589925c0d0f0dcf3b3bbb262d6
Fei Gao <fgaoaa@connect.ust.hk>
2019-09-11 21:05:46 +0800

    Update config_gcc.sh

--- config_gcc.sh
+++ config_gcc.sh
@@ -1,3 +1,3 @@
-sudo apt-get install -y gcc-7 g++-7 & sleep 0.2;
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 & sleep 0.2;
+sudo apt-get install -y gcc-7 g++-7 && sleep 0.2;
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 && sleep 0.2;
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7;
